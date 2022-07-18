d3848d047c53ebc61cbbfde6d303ba277fd88dce
zanyPhi <phedas01@gmail.com>
2022-07-18 12:22:43 +0100

    commit documentations on main.py

--- main.py
+++ main.py
@@ -4,4 +4,22 @@ def git_message():
 git_message()
 
 """Git commands
+
+- cmd commands:
+    - cd <filepath> :move to directory
+    - mkdir <filename/foldername>: create a file or directory
+    - rm <filename/foldername>: remove file or directory
+    - rmdir <filename>: remove file or directory
+    - touch <filenaem> (cmder/linux/mac): create a file or directory
+
+- git:
+    - --version: check git version if installed
+    - config --global user.name <username>: add username
+    - config --global user.email <email>: add user email
+    - init: create new repository
+    - status: check whether files are untracked, staged or commited
+    - add: stage files
+    - add . : stage all files in a repo
+    - commit -m "<message/commit details>": commit staged files with a message
+    
 """
