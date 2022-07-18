f66bfd25037eda7cb5fef82c91b62d6e8b625b24
zanyPhi <phedas01@gmail.com>
2022-07-18 12:28:06 +0100

    created learning_mate class and mate1 object in utils

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
 print('created a second file')
+class Learning_mate:
+    def __init__(self, name, age, mate_name, mate_age):
+        self.name = name
+        self.age = age
+        self.mate_name = mate_name
+        self.mate_age = mate_age
+
+mate1 = Learning_mate('Sam', 20, 'Toby', 20)
