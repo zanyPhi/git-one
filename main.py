526827ad5033e63e2f120413d1be823611f33cf2
zanyPhi <phedas01@gmail.com>
2022-07-19 09:32:55 +0100

    all said and done

--- main.py
+++ main.py
@@ -26,11 +26,12 @@ git_message()
     - commit -m "<message/commit details>": commit staged files with a message
     - log: show commit history
     - log --oneline: show "concise" commit history
-    - **checkout <commit id>: show file at commit stage. changes won't be saved
-    - revert <commit id>: show and undo changes at commit. a text editor window to edit
-            commit message is given. to exit use>> :wq >>press 'enter' 
-    - reset <commit id>: delete all commits after specified commit but changes in files
-            are not reset
+    - *checkout <commit id>: show file at commit stage. changes won't be saved
+    - revert <commit id>: show and undo changes at commit. a text editor 
+        window to edit commit message is given. to exit use>> :wq >>press
+        'enter' 
+    - reset <commit id>: delete all commits after specified commit but changes
+        in files are not reset
     - reset <commit id> --hard: delete all commits after plus file changes 
      - branch <branchname>: create a branch
     - branch -a: list all branches
@@ -38,8 +39,26 @@ git_message()
     - branch -d <branchname>: delete merged branch
     - branch -D <branchname>: delete unmerged branch
     - checkout -b <branchname>: create and switch to branch
-    - merge <branchname>: you must be on master branch to merge
+    - merge <branchname>: you must be on main(master) branch to merge
     - push <remote repo path> <branchname>: to push to remote repo o github
     - remote add <alias> <remote repo path>: create alias for remote repo path
-    - push <alias> <banchname>: push to remote
+    - push <alias> <branchname>: push to remote
+    - clone <remote repo path>: create a local repo from github. will create
+        folder with repository name
+
+    (Collaboration workflow)
+    - pull origin <branch>: update cloned repo
+    - create new branch
+    - stage, commit
+    - push branch, not main
+    - compare and create pull request(to merge with master branch)
+
+
+    (Contribution workflow)
+    - fork on github to your account
+    - clone forked repo
+    - stage, commit
+    - push main on origin
+    - create pull request
+    - only author can merge
 """
